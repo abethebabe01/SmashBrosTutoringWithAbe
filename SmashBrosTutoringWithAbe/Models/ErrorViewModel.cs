4919fb90e3e16885c518996bb738e6183de7b0e9
abethebabe01 <103139893+abethebabe01@users.noreply.github.com>
2024-02-08 19:07:48 -0700

    Add project files.

---
+++ SmashBrosTutoringWithAbe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmashBrosTutoringWithAbe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
